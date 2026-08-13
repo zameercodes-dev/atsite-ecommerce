f581d0b82c813ceddbb96e2b0f8ba22482ff8df9
Zameer <zameerz519@gmail.com>
2026-08-13 13:20:45 +0530

    Delete e-commerce site directory

--- e-commerce site/index.js
+++
@@ -1,9 +0,0 @@
-const toggle_botton= document.querySelector(".navbar-menu-toggle")
-const sidenavbar=document.querySelector(".side-navbar")
-toggle_botton.addEventListener("click",function(){
-    sidenavbar.style.left="0%"
-
-})
-function closenav(){
-    sidenavbar.style.left="-60%"
-}
